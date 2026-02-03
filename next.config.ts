0b64cf0bb992d2fa75cfbc4589a5605104525b72
tninan <tninan.optimus@yobelscm.biz>
2026-02-03 18:44:11 -0500

    Fix: Remove unsupported appIsrStatus property from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   devIndicators: {
-    appIsrStatus: false,
     buildActivity: false,
     buildActivityPosition: 'bottom-right',
   },
